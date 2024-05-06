a44f9bf4ec8feec730f2d2cf174aec4762ea4ac1
Franck HERPOUX <franckherpoux@hotmail.fr>
2024-05-06 09:53:03 +0200

    Route Creation

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,38 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 
+// pages
+
+import HomePage from "./pages/HomePage";
+import ProjectsPage from "./pages/ProjectsPage";
+import ContactPage from "./pages/ContactPage";
+
+// router creation
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/ProjectsPage",
+        element: <ProjectsPage />,
+      },
+      {
+        path: "/ContactPage",
+        element: <ContactPage />,
+      },
+      {},
+    ],
+  },
+]);
+
+// rendering
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
