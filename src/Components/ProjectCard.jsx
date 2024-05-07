70818b3a70f41237327d838f2c49124fba091a12
Franck HERPOUX <franckherpoux@hotmail.fr>
2024-05-07 18:33:24 +0200

    add style on project page

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,34 +1,43 @@
 import TimeTravel from "../assets/Images/TimeTravel.jpg";
 import StarWild from "../assets/Images/StarWild.png";
+import ComingSoon from "../assets/Images/ComingSoon.jpg";
+
+import "./ProjectCard.css";
 
 const Projects = [
   {
     title: "Chronologie de l'Humanitée",
-    description:
-      "Une chronologie interactive retraçant l'histoire de l'humanitée",
+    description: "An interactive timeline showing the history of humanity",
     image: TimeTravel,
     lien: "https://moonlow31.github.io/Projet-Chrono/",
   },
   {
     title: "Star Wild: l'odyssey de la wild",
-    description: "Une exploration spatiale du système solaire un 3D !",
+    description: "A Spatial trip of the solar system in 3D !",
     image: StarWild,
     lien: "https://star-wild.netlify.app/",
+  },
+  {
+    title: "Next Project",
+    description: "",
+    image: ComingSoon,
+    lien: "",
   },
 ];
 
 function ProjectCard() {
   return (
-    <>
+    <div className="AllCards">
       {Projects.map((project, index) => (
         <div className="Card" key={[index]}>
           <h4>{project.title}</h4>
           <p>{project.description}</p>
-          <img src={project.image} alt={project.title} />
-          <a href={project.lien} />
+          <a href={project.lien}>
+            <img src={project.image} alt={project.title} />
+          </a>
         </div>
       ))}
-    </>
+    </div>
   );
 }
 
